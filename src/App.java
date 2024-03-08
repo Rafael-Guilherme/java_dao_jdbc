6c9a1b8c422c1cc5d3412498c1dd5173bc03e1db
Rafael Guilherme <rafael_guilherme@hotmail.com>
2024-03-08 14:59:38 -0300

    feat: model entity Department

--- src/App.java
+++ src/App.java
@@ -1,5 +1,10 @@
+import model.entities.Department;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+
+        Department obj = new Department(1, "Books");
+
+        System.out.println(obj);
     }
 }
