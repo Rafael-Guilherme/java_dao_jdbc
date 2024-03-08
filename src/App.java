a55b5a65f74643c7d86399f9b7985da492d4ff25
Rafael Guilherme <rafael_guilherme@hotmail.com>
2024-03-08 15:35:20 -0300

    feat: SellerDao and DaoFactory

--- src/App.java
+++ src/App.java
@@ -1,5 +1,7 @@
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -9,6 +11,8 @@ public class App {
         Department obj = new Department(1, "Books");
 
         Seller seller = new Seller(21, "Bob", "bob@gmail", new Date(), 3000.00, obj);
+
+        SellerDao sellerDao = DaoFactory.createSellerDao();
 
         System.out.println(obj);
         System.out.println(seller);
